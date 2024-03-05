fc80ce72487eb1c01ba2a17757b8c518aaf374f2
zomasec <ah905167@gmail.com>
2024-03-05 06:02:57 +0200

    add logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,43 @@
 package main
 
 
+import (
 
+	"flag"
+	"github.com/zomasec/logz"
+	"corser/pkg/corser"
+)
+
+
+var logger = logz.DefaultLogs()
+
+
+func main() {
+
+	cLevel := flag.Int("c", 30, "Concurrency level or the number of workers to use.")
+	checkWildcard := flag.Bool("wc", false, "Enable to check the wildcard in Access-Control-Allow-Origin.")
+	header := flag.String("H", "", "Custom header added to each request.")
+	method := flag.String("m", "GET", "Specific method name requested with it in each request.")
+	cookies := flag.String("cookies", "", "Add cookies to each request to access authenticated pages.")
+	list := flag.String("l", "", "List of URLs to scan.")
+	timeout := flag.Int("timeout", 5, "Timeout for each request")
+
+	flag.Parse()
+	var Scan *corser.Scanner
+
+	if *list != "" {
+		if err := Scan.ReadURLsFromFile(*list); err != nil {
+			logger.FATAL("Error reading from a file %s , %v\n", *list, err)
+		}
+
+	} else {
+		if err := Scan.ReadFromStdin(); err != nil {
+			logger.FATAL("Error reading from Stdin")
+		}
+	}
+
+	scan := corser.NewScanner(Scan.URLs, *cLevel, *checkWildcard, corser.NewRequester(*method, *header, *cookies, *timeout))
+
+	scan.RunScan()
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module corser
 go 1.22.0
 
 require (
+	github.com/zomasec/logz v1.0.0 // indirect
 	github.com/zomasec/tld v1.0.2 // indirect
 	golang.org/x/net v0.21.0 // indirect
 )
