b3bd5739d20e1e6215b38ef1b5a54c6af39d6945
h0tak88r <h0tak88r@gmail.com>
2024-04-14 13:02:58 +0200

    Adding option to poc output in a file

--- vulnserver/vulnserver.go
+++ vulnserver/vulnserver.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"regexp"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-// all of these are vulnrable to CORS Misconfiguration
-
-func handleCORS_Wildcard() gin.HandlerFunc {
-	return func(ctx*gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusOK)
-			return
-		}
-		ctx.Next()
-	}
-}
+// Handlers for CORS configurations
 
 func handleCORS_ReflectedOrigin() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -30,7 +16,7 @@ func handleCORS_ReflectedOrigin() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 		ctx.AbortWithStatus(http.StatusOK)
-		
+
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(http.StatusCreated)
 			return
@@ -53,71 +39,19 @@ func handleCORS_Null() gin.HandlerFunc {
 	}
 }
 
-// CORS Middleware
-
-func handleCORS_Rege0x1() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		origin :=  ctx.Request.Header.Get("Origin")
-
-		if wanted, _ := regexp.MatchString(`(https?://)([^/@]+@)?(.*\.)?target\.com`, ""); !wanted {
-            ctx.AbortWithStatus(http.StatusForbidden)
-            return
-
-        }
-
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		
-        if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusOK)
-			return
-		}
-		ctx.Next()
-
-	}
-}
-
-// CORS Middleware
-
 func handleCORS_Rege0x2() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		origin :=  ctx.Request.Header.Get("Origin")
+		origin := ctx.Request.Header.Get("Origin")
 
 		if wanted, _ := regexp.MatchString(`(https?://)([^/@]+@)?(.*\.)?target\.com`, ""); !wanted {
-            ctx.AbortWithStatus(http.StatusForbidden)
-            return
-
-        }
-
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		
-        if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusOK)
+			ctx.AbortWithStatus(http.StatusForbidden)
 			return
 		}
-		ctx.Next()
-
-	}
-}
-
-func handleCORS_WildcardDomain() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-        origin := ctx.Request.Header.Get("Origin")
-        // Check if the origin matches the wildcard pattern
-        if !strings.HasPrefix(origin, "https://") && !strings.HasSuffix(origin, ".target.com") {
-            ctx.AbortWithStatus(http.StatusForbidden)
-            fmt.Println(origin)
-			return
-        }
-        
-     
 
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(http.StatusOK)
 			return
@@ -126,63 +60,51 @@ func handleCORS_WildcardDomain() gin.HandlerFunc {
 	}
 }
 
-
-func handleCORS_Normal() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-        origin := ctx.Request.Header.Get("Origin")
-        if origin != "https://*.target.com" {
-            ctx.AbortWithStatus(http.StatusForbidden)
-			return
-        } 
-
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusOK)
-			return
-		}
-		ctx.Next()
-	}
-}
+// Main function
 
 func main() {
+	// First server instance
 	app := gin.Default()
-
-	// Configure CORS middleware
 	app.Use(handleCORS_ReflectedOrigin())
-
-
 	app.GET("/api/1", func(ctx *gin.Context) {
-		// Handle sensitive data
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Sensitive data processed successfully",
 		})
 	})
+	go func() {
+		if err := app.Run(":1000"); err != nil {
+			log.Fatal("Error starting server 1: ", err)
+		}
+	}()
 
-
+	// Second server instance
 	app2 := gin.Default()
-
 	app2.Use(handleCORS_Null())
 	app2.GET("/api/2", func(ctx *gin.Context) {
-		// Handle sensitive data
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Sensitive data processed successfully",
 		})
 	})
+	go func() {
+		if err := app2.Run(":2000"); err != nil {
+			log.Fatal("Error starting server 2: ", err)
+		}
+	}()
 
+	// Third server instance
 	app3 := gin.Default()
-
 	app3.Use(handleCORS_Rege0x2())
 	app3.GET("/api/3", func(ctx *gin.Context) {
-		// Handle sensitive data
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Sensitive data processed successfully",
 		})
 	})
+	go func() {
+		if err := app3.Run(":3000"); err != nil {
+			log.Fatal("Error starting server 3: ", err)
+		}
+	}()
 
-	// Start the server
-	app.Run(":1000")
-	app2.Run("2000")
-	app3.Run("3000")
+	// Keep the main goroutine running
+	select {}
 }
