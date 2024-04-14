10696b15e10ae7542833fa51ef084557c3ddf789
h0tak88r <h0tak88r@gmail.com>
2024-04-14 13:16:29 +0200

    add file poc option

--- cmd/corser/main.go
+++ cmd/corser/main.go
@@ -1,51 +1,49 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "github.com/zomasec/corser/runner" 
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/zomasec/corser/runner"
 	"github.com/zomasec/corser/utils"
 )
 
-
-
 func main() {
 
-    urlFlag := flag.String("url", "", "Specifies the URL to scan for CORS misconfigurations.")
-    methodFlag := flag.String("method", "GET", "Specifies the HTTP method to use when sending requests.")
-    timeoutFlag := flag.Int("timeout", 5, "Sets the timeout (in seconds) for each request.")
-    clevelFlag := flag.Int("c", 10, "Determines the concurrency level, i.e., the number of concurrent requests to make.")
-    cookieFlag := flag.String("cookie", "", "Defines cookies to include in the scan requests. Format as a single string (e.g., 'sessionId=abc123; token=xyz').")
-    fileFlag := flag.String("l", "", "Specifies a file path containing URLs to scan, with one URL per line.")
-    deepScanFlag := flag.Bool("deep", false, "Enable deepscan will use more advanced teqnueqs to bypass CORS Security messures")
-    originFlag := flag.String("origin", "http://zomasec.io", "Sets the Origin header value to use in the scan requests.")
-    headerFlag := flag.String("header", "", "Specifies additional headers to include in the scan requests. Format as a single string (e.g., 'X-Custom-Header=Value').")
-    verboseFlag := flag.Bool("v", false, "Enable Verbose mode to show errors and detailed logs ")
-    // pocFlag := flag.Bool("poc", false, "Generate poc for any vuln request with the name of the url, result found  ")
-    // pocFileFlag := flag.String("pf")
+	urlFlag := flag.String("url", "", "Specifies the URL to scan for CORS misconfigurations.")
+	methodFlag := flag.String("method", "GET", "Specifies the HTTP method to use when sending requests.")
+	timeoutFlag := flag.Int("timeout", 5, "Sets the timeout (in seconds) for each request.")
+	clevelFlag := flag.Int("c", 10, "Determines the concurrency level, i.e., the number of concurrent requests to make.")
+	cookieFlag := flag.String("cookie", "", "Defines cookies to include in the scan requests. Format as a single string (e.g., 'sessionId=abc123; token=xyz').")
+	fileFlag := flag.String("l", "", "Specifies a file path containing URLs to scan, with one URL per line.")
+	deepScanFlag := flag.Bool("deep", false, "Enable deepscan will use more advanced teqnueqs to bypass CORS Security messures")
+	originFlag := flag.String("origin", "http://zomasec.io", "Sets the Origin header value to use in the scan requests.")
+	headerFlag := flag.String("header", "", "Specifies additional headers to include in the scan requests. Format as a single string (e.g., 'X-Custom-Header=Value').")
+	verboseFlag := flag.Bool("v", false, "Enable Verbose mode to show errors and detailed logs ")
+	pocFileFlag := flag.String("pf", "", "Generate poc for any vuln request with the name of the url, result found  ")
 
-    flag.Parse()
-    // URLs slice to hold either single URL or URLs from the file
-    var urls []string
+	flag.Parse()
+	// URLs slice to hold either single URL or URLs from the file
+	var urls []string
 
-    // Check if the file flag is provided
-    if *fileFlag != "" {
-       urls = append(urls, utils.ReadFileLines(*fileFlag)...)
-    } else if *urlFlag != "" {
-        // Single URL provided
-        urls = append(urls, *urlFlag)
-    } else {
-        fmt.Println("Usage: cors-scanner -url <URL> or cors-scanner -file <file path> [-origin <Origin>] [-headers <Headers>]")
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	// Check if the file flag is provided
+	if *fileFlag != "" {
+		urls = append(urls, utils.ReadFileLines(*fileFlag)...)
+	} else if *urlFlag != "" {
+		// Single URL provided
+		urls = append(urls, *urlFlag)
+	} else {
+		fmt.Println("Usage: cors-scanner -url <URL> or cors-scanner -file <file path> [-origin <Origin>] [-headers <Headers>]")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-    // Run the scanner for the URLs
-    r := runner.NewRunner(urls, *methodFlag, *headerFlag, *originFlag, *cookieFlag,*deepScanFlag, *verboseFlag, *timeoutFlag, *clevelFlag) 
-    err := r.Start()
-    if err != nil {
-        fmt.Printf("Error running scan: %s\n", err)
-        os.Exit(1)
-    }
+	// Run the scanner for the URLs
+	r := runner.NewRunner(urls, *methodFlag, *headerFlag, *originFlag, *cookieFlag, *deepScanFlag, *verboseFlag, *timeoutFlag, *clevelFlag, *pocFileFlag)
+	err := r.Start()
+	if err != nil {
+		fmt.Printf("Error running scan: %s\n", err)
+		os.Exit(1)
+	}
 }
--- runner/runner.go
+++ runner/runner.go
@@ -1,16 +1,16 @@
 package runner
 
 import (
-	"github.com/zomasec/corser/pkg/corser" // Ensure this import path matches your project structure
-	"github.com/zomasec/logz"
-	//"github.com/zomasec/corser/pkg/pocgen"
 	"fmt"
 	"sync"
+
+	"github.com/zomasec/corser/pkg/corser"
+	"github.com/zomasec/corser/pkg/pocgen"
+	"github.com/zomasec/logz"
 )
 
 var (
 	logger = logz.DefaultLogs()
-	
 )
 
 // Runner coordinates scans, now also includes origin and headers for customization.
@@ -24,10 +24,11 @@ type Runner struct {
 	Timeout  int
 	CLevel   int
 	Header   string
+	PocFile  string
 }
 
 // NewRunner creates a new Runner instance capable of scanning multiple URLs with custom settings.
-func NewRunner(urls []string, method, header, origin, cookies string, isDeep, verbose bool, timeout, cLevel int) *Runner {
+func NewRunner(urls []string, method, header, origin, cookies string, isDeep, verbose bool, timeout, cLevel int, pocFile string) *Runner {
 	return &Runner{
 		URLs:     urls,
 		Origin:   origin,
@@ -36,15 +37,16 @@ func NewRunner(urls []string, method, header, origin, cookies string, isDeep, ve
 		Timeout:  timeout,
 		CLevel:   cLevel,
 		DeepScan: isDeep,
-		Verbose: verbose,
+		Verbose:  verbose,
 		Header:   header,
+		PocFile:  pocFile,
 	}
 }
 
 // Start begins the scanning process for all provided URLs with the specified origin and headers.
 func (r *Runner) Start() error {
 	var wg sync.WaitGroup
-	clevel := make(chan struct{}, r.CLevel) // Control the concurrency level
+	clevel := make(chan struct{}, r.CLevel)
 
 	logger.ErrorEnabled = r.Verbose
 	logger.DebugEnabled = r.Verbose
@@ -55,18 +57,33 @@ func (r *Runner) Start() error {
 
 		go func(u string) {
 			defer wg.Done()
-			defer func() { <-clevel }() // Release the slot
+			defer func() { <-clevel }()
 
-			// Assuming NewScanner's correct parameters are url and origin for simplicity
-			// Adjust parameters as per your actual NewScanner function
 			scanner := corser.NewScanner(u, r.Method, r.Header, r.Origin, r.Origin, r.DeepScan, r.Timeout)
 			result := scanner.Scan()
 
-			// Displaying details about the scan results
 			if result.Vulnerable && len(result.Details) > 0 {
 				logz.NewLogger("vuln", logz.Blue, result.URL).Log()
 				for _, detail := range result.Details {
-					fmt.Printf("\t %s-%s %s%s%s\n", logz.Yellow, logz.NC ,logz.Green ,detail, logz.NC)
+					fmt.Printf("\t %s-%s %s%s%s\n", logz.Yellow, logz.NC, logz.Green, detail, logz.NC)
+				}
+
+				// Generate PoC HTML and save it to a file
+				if r.PocFile != "" {
+					pocConfig := &pocgen.Config{
+						Method:           r.Method,
+						TargetURL:        u,
+						SetRequestHeader: r.Header,
+						CustomOrigin:     r.Origin,
+					}
+
+					filename := r.PocFile
+
+					if err := pocgen.SavePoCToFile(pocConfig, filename); err != nil {
+						logger.ERROR("Error generating PoC: %v", err)
+					} else {
+						logger.INFO("PoC saved to %s", filename)
+					}
 				}
 			}
 
@@ -76,7 +93,6 @@ func (r *Runner) Start() error {
 
 		}(url)
 	}
-	
 
 	wg.Wait()
 	return nil
